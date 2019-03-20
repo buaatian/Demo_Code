4be55df347b65e44310ece51d85635ad384335a2
tianwang <tianwang@buaa.edu.cn>
2019-03-20 18:13:39 +0800

    Chapter_6

---
+++ pages/Chapter_4/frontend/frontend.js
@@ -0,0 +1,80 @@
+// pages/Chapter_4/frontend/frontend.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    id:"null",
+    name:"null"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    const page = this;
+    wx.request({
+      url: 'http://localhost/backend/response.php',
+      data:{
+        id:"001"
+      },
+      success(res) {
+        console.log(res)
+        page.setData({
+          id:res.data.id,
+          name:res.data.name
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
