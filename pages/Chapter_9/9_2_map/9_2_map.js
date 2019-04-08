5cb4517243bc531e302855c9fa82d2ab5d45b966
tianwang <tianwang@buaa.edu.cn>
2019-04-08 22:05:22 +0800

    map_modify

--- pages/Chapter_9/9_2_map/9_2_map.js
+++ pages/Chapter_9/9_2_map/9_2_map.js
@@ -8,15 +8,6 @@ Page({
       longitude: 113.324520,
       name: 'T.I.T 创意园'
     }],
-    covers: [{
-      latitude: 23.099994,
-      longitude: 113.344520,
-      iconPath: '/image/location.png'
-    }, {
-      latitude: 23.099994,
-      longitude: 113.304520,
-      iconPath: '/image/location.png'
-    }],
     polygons: [{
       points: [
         {
